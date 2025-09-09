1b7064a64d4891549c7dcc85bf4a346fe0b91c62
NashwaK <nashwakvr@gmail.com>
2025-09-09 23:11:45 +0530

    home

--- lib/ui/home/bind/home_bind.dart
+++ lib/ui/home/bind/home_bind.dart
@@ -13,48 +13,61 @@ class HomeBind implements Bindings {
 class HomeController extends GetxController {
   static HomeController get to => Get.find();
 
-  // PageController for the main movie carousel
-  late PageController pageController;
-  // Tracks the current page for the scaling animation
-  final RxDouble currentPage = 0.0.obs;
-
-  // Dummy data for the movie lists
-  final RxList<Movie> bookNowMovies = <Movie>[
-    Movie(title: 'The Home', posterUrl: 'https://i.ebayimg.com/images/g/sLgAAOSw2c5b42im/s-l1600.jpg'),
-    Movie(title: 'Superman', posterUrl: 'https://m.media-amazon.com/images/I/71uL-10mhXL.jpg'),
-    Movie(title: 'Formula 1', posterUrl: 'https://www.themoviedb.org/t/p/original/s25aV3tY30a9u0m0iCV2l3qH22r.jpg'),
-  ].obs;
-
-  final RxList<Movie> trendingMovies = <Movie>[
-    Movie(title: 'Gravity', posterUrl: 'https://m.media-amazon.com/images/I/A1nFk5K5b-L._AC_SL1500_.jpg'),
-    Movie(title: 'The Invisible', posterUrl: 'https://i.pinimg.com/originals/82/c4/cc/82c4cc34a4a159511516094a9740523d.jpg'),
-    Movie(title: 'Big Four', posterUrl: 'https://m.media-amazon.com/images/M/MV5BNTM4ZmNjYmYtNjVjMS00YjVmLWJiMGEtNjMwN2EzN2FmNTE5XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg'),
-  ].obs;
-
-  final RxList<Movie> upcomingMovies = <Movie>[
-    Movie(title: 'Jurassic World', posterUrl: 'https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_FMjpg_UX1000_.jpg'),
-    Movie(title: 'Serenity', posterUrl: 'https://m.media-amazon.com/images/I/A1ed25y6LFL._AC_SL1500_.jpg'),
-    Movie(title: 'Downton Abbey', posterUrl: 'https://www.uphe.com/sites/default/files/styles/scale__344w_/public/2021/01/DowntonAbbey_PosterArt.jpg'),
-  ].obs;
-
+  final PageController pageViewController =
+  PageController(viewportFraction: 0.5);
+  double currentPage = 0;
 
   @override
   void onInit() {
-    super.onInit();
-    // Initialize PageController with a viewport fraction for peeking effect
-    pageController = PageController(viewportFraction: 0.7, initialPage: 1);
-    // Listen to page changes to update the currentPage reactive variable
-    pageController.addListener(() {
-      currentPage.value = pageController.page!;
+    pageViewController.addListener(() {
+        currentPage = pageViewController.page ?? 0;
+        update();
     });
+    super.onInit();
   }
 
-  @override
-  void onClose() {
-    pageController.dispose();
-    super.onClose();
-  }
-}
+  // PageController for the main movie carousel
+//   late PageController pageController;
+//   // Tracks the current page for the scaling animation
+//   final RxDouble currentPage = 0.0.obs;
+//
+//   // Dummy data for the movie lists
+//   final RxList<Movie> bookNowMovies = <Movie>[
+//     Movie(title: 'The Home', posterUrl: 'https://i.ebayimg.com/images/g/sLgAAOSw2c5b42im/s-l1600.jpg'),
+//     Movie(title: 'Superman', posterUrl: 'https://m.media-amazon.com/images/I/71uL-10mhXL.jpg'),
+//     Movie(title: 'Formula 1', posterUrl: 'https://www.themoviedb.org/t/p/original/s25aV3tY30a9u0m0iCV2l3qH22r.jpg'),
+//   ].obs;
+//
+//   final RxList<Movie> trendingMovies = <Movie>[
+//     Movie(title: 'Gravity', posterUrl: 'https://m.media-amazon.com/images/I/A1nFk5K5b-L._AC_SL1500_.jpg'),
+//     Movie(title: 'The Invisible', posterUrl: 'https://i.pinimg.com/originals/82/c4/cc/82c4cc34a4a159511516094a9740523d.jpg'),
+//     Movie(title: 'Big Four', posterUrl: 'https://m.media-amazon.com/images/M/MV5BNTM4ZmNjYmYtNjVjMS00YjVmLWJiMGEtNjMwN2EzN2FmNTE5XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg'),
+//   ].obs;
+//
+//   final RxList<Movie> upcomingMovies = <Movie>[
+//     Movie(title: 'Jurassic World', posterUrl: 'https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_FMjpg_UX1000_.jpg'),
+//     Movie(title: 'Serenity', posterUrl: 'https://m.media-amazon.com/images/I/A1ed25y6LFL._AC_SL1500_.jpg'),
+//     Movie(title: 'Downton Abbey', posterUrl: 'https://www.uphe.com/sites/default/files/styles/scale__344w_/public/2021/01/DowntonAbbey_PosterArt.jpg'),
+//   ].obs;
+//
+//
+//   @override
+//   void onInit() {
+//     super.onInit();
+//     // Initialize PageController with a viewport fraction for peeking effect
+//     pageController = PageController(viewportFraction: 0.7, initialPage: 1);
+//     // Listen to page changes to update the currentPage reactive variable
+//     pageController.addListener(() {
+//       currentPage.value = pageController.page!;
+//     });
+//   }
+//
+//   @override
+//   void onClose() {
+//     pageController.dispose();
+//     super.onClose();
+//   }
+// }
 // class HomeController extends GetxController {
 //   static HomeController get to => Get.find();
 //
@@ -62,4 +75,4 @@ class HomeController extends GetxController {
 //   void onInit() {
 //     super.onInit();
 //   }
-// }
+}
--- lib/ui/home/view/more.dart
+++ lib/ui/home/view/more.dart
@@ -1,4 +1,225 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_utils/flutter_custom_utils.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:get/get_state_manager/src/simple/get_state.dart';
+import 'package:movie_mate/res/colors.dart';
+import 'package:movie_mate/res/images.dart';
+import 'package:movie_mate/res/style.dart';
+import 'package:movie_mate/ui/home/bind/home_bind.dart';
+
+class FirstPart extends StatelessWidget {
+  const FirstPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        // Background Image
+        Container(
+          height: context.cHeight * 0.4,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(homeBanner),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+
+        // Dark overlay (tint)
+        Container(
+          height: context.cHeight * 0.4,
+          width: double.infinity,
+          color: Colors.black.withOpacity(0.3),
+        ),
+
+        // Bottom gradient fade
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            height: 80,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.transparent,
+                  primaryColor,
+                ],
+              ),
+            ),
+          ),
+        ),
+
+        // Top Content
+        Positioned.fill(
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: TextfieldPart(),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TextfieldPart extends StatelessWidget {
+  const TextfieldPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: TextField(
+        style: const TextStyle(color: Colors.white),
+        decoration: InputDecoration(
+          hintText: 'Search Movie',
+          hintStyle: TextStyle(color: Colors.white.withOpacity(0.5)),
+          prefixIcon: Icon(Icons.search, color: Colors.white.withOpacity(0.5)),
+          filled: true,
+          fillColor: Colors.white.withOpacity(0.25),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30.0),
+            borderSide: BorderSide.none,
+          ),
+          contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+        ),
+      ),
+    );
+  }
+}
+
+class ProductsPart extends StatelessWidget {
+  const ProductsPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<HomeController>(
+      builder: (logic) {
+        return SizedBox(
+          height: 220, // enough height for scaling
+          child: PageView.builder(
+            controller: logic.pageViewController,
+            itemCount: 10,
+            physics: const BouncingScrollPhysics(),
+            itemBuilder: (context, index) {
+              // calculate scale for center item
+              double scale = 1.0;
+              scale = (index == logic.currentPage.floor() || index == logic.currentPage.ceil())
+                  ? 1.0 + (1 - (logic.currentPage - index).abs()).clamp(0.0, 1.0) * 0.3
+                  : 1.0;
+
+              return TweenAnimationBuilder(
+                duration: const Duration(milliseconds: 200),
+                tween: Tween<double>(begin: 1, end: scale),
+                builder: (context, value, child) {
+                  return Transform.scale(
+                    scale: value,
+                    child: child,
+                  );
+                },
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    image: DecorationImage(
+                      image: AssetImage(homeBanner),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Stack(
+                    children: [
+                      // Bottom button
+                      Positioned(
+                        bottom: 30,
+                        left: 0,
+                        right: 0,
+                        child: Center(
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Text(
+                              'data',
+                              style: customStyle(14, Colors.black, FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+}
+
+
+// class ProductsPart extends StatelessWidget {
+//   const ProductsPart({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetBuilder<HomeController>(
+//         builder: (logic) {
+//           return SizedBox(
+//             height: 190, // fix height for your product cards
+//             child: MasonryGridView.count(
+//               crossAxisCount: 1, // single row
+//               scrollDirection: Axis.horizontal,
+//               mainAxisSpacing: 12,
+//               crossAxisSpacing: 12,
+//               padding: EdgeInsets.symmetric(horizontal: 12),
+//               itemCount: 10,
+//               itemBuilder: (context, i) {
+//                 return Container(
+//                   width: 150,
+//                   decoration: BoxDecoration(
+//                     borderRadius: BorderRadius.circular(10),
+//                     image: DecorationImage(
+//                       image: AssetImage(homeBanner),
+//                       fit: BoxFit.cover,
+//                     ),
+//                   ),
+//                 );
+//               },
+//             ),
+//           );
+//         }
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class BackColorLeft extends StatelessWidget {
   const BackColorLeft({super.key});
